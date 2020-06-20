0d9ceac98b0c6d50b7f399685635aa9a51953f2a
Andrii Kurdiumov <kant2002@gmail.com>
2020-06-20 16:24:50 +0600

    Add project for show case M.E.Configuration

---
+++ MEConfigurationSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace MEConfigurationSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: false)
+                .AddIniFile("config.ini", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+            Console.WriteLine("Press Ctrl+C to stop. Enter to iterate configuration read one more time.");
+            while (true)
+            {
+                Console.WriteLine($"Hello {configRoot["Greetings:Name"]}");
+                Console.ReadLine();
+            }
+        }
+    }
+}
